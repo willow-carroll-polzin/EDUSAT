7e8b76e651a4ec34afdbe8b80558a499c692651b
Keyanna Coghlan <keyanna.coghlan@gmail.com>
2020-08-16 17:44:22 -0400

    set up structure of connect react-redux to the html of the webpage

--- EDUSAT_App/src/main.tsx
+++ EDUSAT_App/src/main.tsx
@@ -1,8 +1,12 @@
 /*  REQUIRED LIBRARIES  */
 import { app, BrowserWindow } from "electron";
+import React from "react"
+import ReactDOM from "react-dom"
+import {connect} from "react-redux"
 import * as path from "path";
 import SerialPort from "serialport";
 import { createStore } from "redux";
+import {Provider} from "react-redux"
 import {
   SensorStatus,
   State,
@@ -10,47 +14,9 @@ import {
   UpdateSensorData,
   UpdateComPort,
 } from "./interfaces";
+import {SectionOneConnected,SectionTwoConnected,SectionThreeConnected} from "./App"
 
-//////////////////////////
-/*      APP SETUP       */
-function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-    width: 800,
-  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", () => {
-  createWindow();
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
 //////////////////////////
 /*      STATE MANAGEMENT        */
 //Redux dispatch store setup
@@ -127,6 +93,71 @@ function reducer(state: State = initial_State, action: Action): State {
   }
   return state;
 }
+
+//CONNECTING REACT-REDUX TO THE HTML
+//These sections will get placed wherever the div with the matching id is set up in index.html
+const sectionOneElement = document.getElementById("sectionOneName");
+ReactDOM.render(
+  <Provider store={store}>
+    <SectionOneConnected />
+  </Provider>,
+  sectionOneElement
+)
+const sectionTwoElement = document.getElementById("sectionTwoName");
+ReactDOM.render(
+  <Provider store={store}>
+    <SectionTwoConnected />
+  </Provider>,
+  sectionTwoElement
+)
+const sectionThreeElement = document.getElementById("sectionThreeName");
+ReactDOM.render(
+  <Provider store={store}>
+    <SectionThreeConnected />
+  </Provider>,
+  sectionThreeElement
+)
+
+/*      APP SETUP       */
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    width: 800,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
 //////////////////////////
 /*      SETUP SERIAL PORT      */
 const HEADER = "H";
